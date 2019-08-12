85b588f36addeab77107dac0e85f7c9077e00e86
faithfollansbee <faithfollansbee@gmail.com>
2019-08-12 17:00:05 -0400

    Removed a slash

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://obscure-bayou-78598.herokuapp.com/',
+  production: 'https://obscure-bayou-78598.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
